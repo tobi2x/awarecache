1ab1ad322e422058b0e0d01a48d6145b063a6a4f
tobi2x <philayodeji07@gmail.com>
2024-08-18 15:42:38 -0400

    Updated Readme

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='awarecache',
-    version='1.5.5',
+    version='1.5.6',
     packages=find_packages(),
     install_requires=[],
     tests_require=['pytest'],
