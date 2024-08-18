28db503a8756be2a2151c7d8704dc606625b4a35
tobi2x <philayodeji07@gmail.com>
2024-08-18 16:00:48 -0400

    Updated cache.py to use correct relative paths

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='awarecache',
-    version='1.5.7',
+    version='1.5.8',
     packages=find_packages(include=['awarecache', 'awarecache.*']),
     install_requires=[],
     tests_require=['pytest'],
